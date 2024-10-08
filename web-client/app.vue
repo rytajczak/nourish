97dc9f43db4895410a948fb60a46536c01c73e89
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-08 17:13:22 -0600

    add nuxt pages

--- web-client/app.vue
+++ web-client/app.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Nourish Meal Planner",
+  description: "A meal planning app to save you time",
+  ogTitle: "Nourish Meal Planner",
+  ogDescription: "A meal planning app to save you time",
+  ogImage: "[og:image]",
+  ogUrl: "[og:url]",
+  twitterTitle: "[twitter:title]",
+  twitterDescription: "[twitter:description]",
+  twitterImage: "[twitter:image]",
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
+</script>
+
 <template>
-  <div></div>
+  <NuxtPage></NuxtPage>
 </template>
