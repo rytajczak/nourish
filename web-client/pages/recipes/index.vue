186ce496320c74fa3926961cbfb7646833528e0f
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-10 08:24:15 -0600

    add auth middleware to protected enpoints

--- web-client/pages/recipes/index.vue
+++ web-client/pages/recipes/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+definePageMeta({ middleware: "auth", auth: { guestRedirectTo: "/login" } });
+</script>
+
 <template>
   <div>a searchable list of recipes</div>
 </template>
--- web-client/pages/recipes/new.vue
+++ web-client/pages/recipes/new.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+definePageMeta({ middleware: "auth", auth: { guestRedirectTo: "/login" } });
+</script>
+
 <template>
   <div>an interface for creating new recipes</div>
 </template>
