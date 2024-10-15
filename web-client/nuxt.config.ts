8b6ceca1b40f7677dc4b48d1bcf72d6d068d45a1
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-15 08:12:58 -0600

    disable ssr

--- web-client/nuxt.config.ts
+++ web-client/nuxt.config.ts
@@ -1,21 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@hebilicious/authjs-nuxt"],
-  runtimeConfig: {
-    authJs: {
-      secret: process.env.NUXT_NEXTAUTH_SECRET,
-    },
-    google: {
-      clientId: process.env.NUXT_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NUXT_GOOGLE_CLIENT_SECRET,
-    },
-    public: {
-      authJs: {
-        baseUrl: process.env.NUXT_NEXTAUTH_URL,
-        verifyClientOnEveryRequest: false,
-      },
-    },
-  },
+  modules: ["@nuxtjs/tailwindcss"],
+  ssr: false,
 });
