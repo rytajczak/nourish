aa6d218f61142f08858abedef24cfcd9e6f1fe5d
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-17 10:02:20 -0600

    update nitro config for aws deployment

--- web-client/nuxt.config.ts
+++ web-client/nuxt.config.ts
@@ -2,5 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  ssr: false,
+  nitro: {
+    preset: "aws-lambda",
+  },
 });
