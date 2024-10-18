7c3f3cf41bb55f31e0356b51465df15702cd00dd
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-18 01:25:19 -0600

    create recipe card for rendering fetched recipes

--- web-client/components/RecipeCard.vue
+++ web-client/components/RecipeCard.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 const props = defineProps<{
-  id: number
-  title: string
-  image: string
-  imageType: string
-}>()
+  id: number;
+  title: string;
+  image: string;
+  imageType: string;
+}>();
 </script>
 
 <template>
-  <div>{{ props.title }}</div>
-  <NuxtImg :src="image" />
+  <div class="flex rounded-3xl bg-[#f7f7f9] p-2">
+    <NuxtImg
+      :src="image"
+      alt="image failed to load"
+      class="rounded-3xl"
+    ></NuxtImg>
+    <p class="p-4">{{ title }}</p>
+  </div>
 </template>
