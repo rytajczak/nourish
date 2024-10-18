3db5ba533f747f1c5c4f40e08b93586007a84e16
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-17 22:45:55 -0600

    create recipe card

---
+++ web-client/components/RecipeCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id: number
+  title: string
+  image: string
+  imageType: string
+}>()
+</script>
+
+<template>
+  <div>{{ props.title }}</div>
+  <NuxtImg :src="image" />
+</template>
