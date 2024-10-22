fece9c5651059a7393e6e2526ccaf5d7d789a113
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-21 22:51:07 -0600

    create user types

--- services/user/types.go
+++ services/user/types.go
@@ -1,1 +1,10 @@
 package main
+
+type RegisterDetails struct {
+	Email        string   `json:"email"`
+	Username     string   `json:"username"`
+	FirstName    string   `json:"firstName"`
+	LastName     string   `json:"lastName"`
+	Diet         string   `json:"diet"`
+	Intolerences []string `json:"intolerences"`
+}
