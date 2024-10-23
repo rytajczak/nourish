f26b26da2ad9535981c4292722cc5b3f5afa63ea
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-23 17:44:40 -0600

    add auth middleware for routing

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-definePageMeta({
-  middleware: "auth",
-});
+// definePageMeta({
+//   middleware: "auth",
+// });
 
 const query = ref("");
 const previousQuery = ref("");
