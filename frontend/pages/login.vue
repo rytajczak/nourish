321e9320941d8a3f00553cf61436100e0f5ef467
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-23 12:12:42 -0600

    update login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,1 +1,19 @@
-<template>login</template>
+<script setup lang="ts">
+const email = ref("");
+const password = ref("");
+
+async function handleSumbit() {
+  console.log(email.value);
+  console.log(password.value);
+}
+</script>
+
+<template>
+  <h1>Login</h1>
+  <form @submit.prevent="handleSumbit">
+    <input type="email" v-model="email" /><br />
+    <input type="password" v-model="password" /><br />
+    <button type="submit">login</button>
+  </form>
+  <a href="/api/auth/google">login with google</a>
+</template>
