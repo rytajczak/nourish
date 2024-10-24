5837046c604ceb6612380e4a14d30d15f09e5f72
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-23 23:52:27 -0600

    implement garbage fix
    what the fuck? what the actual fuck this library sucks

--- services/user/types.go
+++ services/user/types.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 type SignUpDetails struct {
 	Email        string   `json:"email"`
 	Provider     string   `json:"provider"`
@@ -8,13 +13,37 @@ type SignUpDetails struct {
 	FirstName    string   `json:"firstName"`
 	LastName     string   `json:"lastName"`
 	Diet         string   `json:"diet"`
-	Intolerences []string `json:"intolerences"`
+	Intolerances []string `json:"intolerances"`
 }
 
 type Profile struct {
-	ID        string `json:"id"`
-	Username  string `json:"username"`
-	FirstName string `json:"firstName" db:"first_name"`
-	LastName  string `json:"lastName" db:"last_name"`
-	Diet      string `json:"diet"`
+	ID           string      `json:"id"`
+	Username     string      `json:"username"`
+	FirstName    string      `json:"firstName" db:"first_name"`
+	LastName     string      `json:"lastName" db:"last_name"`
+	Diet         string      `json:"diet"`
+	Intolerances StringArray `json:"intolerances"`
+}
+
+// wow this sucks! (https://github.com/jmoiron/sqlx/issues/578)
+type StringArray []string
+
+func (s *StringArray) Scan(value interface{}) error {
+	if value == nil {
+		*s = StringArray{}
+		return nil
+	}
+
+	// Ensure the value is of type []byte (string in PostgreSQL)
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to scan StringArray: %v", value)
+	}
+
+	// Remove the surrounding braces and split by comma
+	str := string(bytes)
+	str = str[1 : len(str)-1] // remove { and }
+	*s = StringArray(strings.Split(str, ","))
+
+	return nil
 }
