ed5b056041ed4284371037c46f3d96f483c6b3c8
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-28 13:46:25 -0600

    remove aws lambda preset

--- packages/frontend/nuxt.config.ts
+++ packages/frontend/nuxt.config.ts
@@ -2,15 +2,15 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: "2024-04-03",
+
   devtools: { enabled: true },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxt/image",
     "@vueuse/nuxt",
     "nuxt-auth-utils",
   ],
-  nitro: {
-    preset: "aws-lambda",
-  },
+
+  compatibilityDate: "2024-10-28",
 });
