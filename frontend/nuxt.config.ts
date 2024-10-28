3a29b837d7c56eea88219b72e07e265f3edaaf77
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-28 17:13:17 -0600

    add shadcn-vue to project

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   future: {
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./app/components/ui",
+  },
 });
