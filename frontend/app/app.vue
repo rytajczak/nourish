648cb9065cbc05ff60d5543ad7ddf2d73057076a
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-28 21:34:06 -0600

    recreate nuxt 4 app router

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,7 +3,5 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <a href="/api/auth/google">sign in with google</a><br />
-    <a href="/api/auth/github">sign in with github</a>
   </div>
 </template>
---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,4 @@
+<template>
+  <a href="/api/auth/google">sign in with google</a><br />
+  <a href="/api/auth/github">sign in with github</a>
+</template>
