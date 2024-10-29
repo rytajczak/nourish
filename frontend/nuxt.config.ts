c2c9e8b65254318d3a5acd5d3c6788e68d79924f
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-29 01:02:44 -0600

    remove icon import
    its bundled with the module anyways

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-auth-utils", "@nuxt/icon", "@nuxt/ui"],
+  modules: ["nuxt-auth-utils", "@nuxt/ui"],
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
   compatibilityDate: "2024-04-03",
