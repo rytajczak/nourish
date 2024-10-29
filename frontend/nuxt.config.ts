f07fb08fa87af1c85405f11074dd07b82e6892ce
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-29 14:57:19 -0600

    implement dumb tailwind fix

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   modules: ["nuxt-auth-utils", "@nuxt/ui"],
+  css: ["./app/assets/global.css"],
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
   compatibilityDate: "2024-04-03",
