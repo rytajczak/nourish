6ca2a38384e474b52e96e4f921bf3428bb669a4e
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-29 14:58:22 -0600

    update package lock

---
+++ frontend/app/pages/home.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: "auth",
+});
+const { loggedIn, user, session, fetch, clear } = useUserSession();
+</script>
+
+<template>
+  <ClientOnly>
+    <UContainer class="flex">
+      <UCard>
+        <template #header>Monday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>Tuesday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>Wednesday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>Thursday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>Friday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>Saturday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>Sunday</template>
+        put meal stuff here
+        <template #footer>
+          <UButton color="neutral">Edit</UButton>
+        </template>
+      </UCard>
+    </UContainer>
+  </ClientOnly>
+</template>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts">
-navigateTo("/signup");
-</script>
-
-<template>you shouldn't be here... yet...</template>
+<template>
+  <div class="flex w-screen p-4">
+    <div></div>
+    <UPopover>
+      <UButton color="neutral" label="Open">Get Started</UButton>
+      <template #content>
+        <UCard>
+          <template #header
+            ><span class="font-bold">Login via provider</span></template
+          >
+          <div class="flex flex-col items-center">
+            <UButton
+              color="neutral"
+              icon="logos:google-icon"
+              to="/api/auth/google"
+              label="Sign up with Google"
+              external
+              class="mb-4"
+              >Sign up with Google</UButton
+            >
+            <UButton
+              color="neutral"
+              icon="i-simple-icons-github"
+              to="/api/auth/google"
+              label="Sign up with Google"
+              external
+              >Sign up with Google</UButton
+            >
+          </div>
+        </UCard>
+      </template>
+    </UPopover>
+  </div>
+</template>
