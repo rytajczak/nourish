806debc9a83a87c98391181acdddb89f72e35b2f
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-10-29 22:09:53 -0600

    add basic profile page

---
+++ frontend/app/pages/profile.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { user } = useUserSession();
+const { data: profile } = await useFetch("/api/profiles");
+</script>
+
+<template>
+  <pre>user: {{ user }}</pre>
+  <pre>data: {{ profile }}</pre>
+</template>
