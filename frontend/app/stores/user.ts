49a42f5c97ba6d1d342a06e54b806dd519cfe96f
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-09 01:23:55 -0700

    init user store

---
+++ frontend/app/stores/user.ts
@@ -0,0 +1,23 @@
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const profile = ref<Profile | null>(null);
+    const intolerances = ref<string[]>([]);
+    const savedRecipes = ref<any[]>([]);
+
+    const setUser = (user: any) => {
+      profile.value = user.profile;
+      intolerances.value = user.intolerances;
+      savedRecipes.value = user.savedRecipes;
+    };
+
+    const logOut = () => {
+      profile.value = null;
+      intolerances.value = [];
+      savedRecipes.value = [];
+    };
+
+    return { profile, intolerances, savedRecipes, setUser, logOut };
+  },
+  { persist: true },
+);
