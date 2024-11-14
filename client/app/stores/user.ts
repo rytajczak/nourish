f1cd6f4d4cbb61383dc4eb433645f549bab2bcc0
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-14 00:23:31 -0700

    simplify user store

--- client/app/stores/user.ts
+++ client/app/stores/user.ts
@@ -1,14 +1,21 @@
+import type { RecipePreview } from "~~/server/api/recipes/search.get";
+
 export const useUserStore = defineStore(
   "user",
   () => {
     const { user, clear } = useUserSession();
+    const intolerances = ref<string[]>([]);
+    const savedRecipes = ref<RecipePreview[]>([]);
+    const dislikedIngredients = ref<string[]>([]);
 
-    const logOut = async () => {
+    async function logOut() {
       await clear();
-      return navigateTo("/");
-    };
+      navigateTo("/");
+    }
 
-    return { user, logOut };
+    async function fetchSavedRecipes() {}
+
+    return { user, intolerances, dislikedIngredients, savedRecipes, logOut };
   },
   { persist: true },
 );
