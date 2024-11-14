c99de06ad110d26d299bdc65618353e9c940a707
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-14 00:23:38 -0700

    remove seo metadata

--- client/app/pages/profile.vue
+++ client/app/pages/profile.vue
@@ -3,26 +3,11 @@ definePageMeta({
   middleware: "auth",
 });
 
-const { profile, intolerances } = useUserStore();
-
-useSeoMeta({
-  title: `${profile?.username} | Profile`,
-  description: `${profile?.username} | Profile`,
-  ogTitle: `${profile?.username} | Profile`,
-  ogDescription: `${profile?.username} | Profile`,
-  ogImage: `${profile?.picture}`,
-  ogUrl: `${profile?.username} | Profile`,
-  twitterTitle: `${profile?.username} | Profile`,
-  twitterDescription: `${profile?.username} | Profile`,
-  twitterImage: `${profile?.picture}`,
-  twitterCard: "summary",
-});
+const { user } = useUserStore();
 </script>
 
 <template>
   <div>
-    <NuxtImg :src="profile?.picture ?? ''" />
-    <pre>{{ profile }}</pre>
-    <pre>{{ intolerances }}</pre>
+    <pre>{{ user }}</pre>
   </div>
 </template>
