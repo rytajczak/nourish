61e74d5712c577af7f0a838842aeafebc1c7e859
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-14 00:23:45 -0700

    redo google auth

--- client/server/api/auth/google.get.ts
+++ client/server/api/auth/google.get.ts
@@ -1,49 +1,43 @@
-const config = useRuntimeConfig();
+import { User } from "#auth-utils";
 
-interface UserResponse extends Record<string, string> {
-  id: string;
-  username: string;
-  email: string;
-  provider: string;
-  picture: string;
-  diet: string;
-}
+const config = useRuntimeConfig();
 
 /**
  * @description Handles the Google OAuth login and redirects to the planner page.
  */
 export default defineOAuthGoogleEventHandler({
   async onSuccess(event, { user, tokens }) {
-    const existingUser = await $fetch<UserResponse>(
-      `${config.public.apiUrl}/users/me`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${tokens.id_token}`,
-        },
-      },
-    );
-    if (existingUser.msg === "user not found") {
-      const newUser = await $fetch<UserResponse>(
-        `${config.public.apiUrl}/users/`,
+    setCookie(event, "idToken", tokens.id_token, {
+      httpOnly: true,
+      path: "/",
+    });
+    try {
+      const existingUser = await $fetch<User>(
+        `${config.public.apiUrl}/users/me`,
         {
-          method: "POST",
+          method: "GET",
           headers: {
             Authorization: `Bearer ${tokens.id_token}`,
           },
-          body: {
-            email: user.email,
-            username: user.name,
-            picture: user.picture,
-            provider: "google",
-          },
         },
       );
+      await setUserSession(event, { user: existingUser });
+      return sendRedirect(event, "/planner");
+    } catch (error) {
+      const newUser = await $fetch<User>(`${config.public.apiUrl}/users/`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${tokens.id_token}`,
+        },
+        body: {
+          email: user.email,
+          username: user.name,
+          picture: user.picture,
+          provider: "google",
+        },
+      });
       await setUserSession(event, { user: newUser });
       return sendRedirect(event, "/onboarding");
     }
-
-    await setUserSession(event, { user: existingUser });
-    return sendRedirect(event, "/onboarding");
   },
 });
