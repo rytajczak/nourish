3d62d1766c15cd8d29a30b745334cf5dc3a2071c
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-15 15:39:27 -0700

    update styling

---
+++ client/app/components/RecipeCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Recipe } from "~~/server/api/recipes/search.get";
+const props = defineProps<Recipe>();
+</script>
+
+<template>
+  <UCard :ui="{ header: 'p-0 sm:p-0' }">
+    <template #header>
+      <NuxtImg
+        :src="props.image"
+        sizes="100vw md:50vw lg:33vw"
+        placeholder
+        preload
+        class="rounded-lg"
+      />
+    </template>
+  </UCard>
+</template>
