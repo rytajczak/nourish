b054e6a7f1eaedabc5cc5b1d596be3e989904e55
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-17 12:52:23 -0700

    update types

--- client/types/auth.d.ts
+++ client/types/auth.d.ts
@@ -1,17 +1,12 @@
 declare module "#auth-utils" {
   interface User {
-    id: string;
-    username: string;
     email: string;
+    username: string;
     provider: string;
     picture: string;
-    diet: string | null;
-    calories: number | null;
-    carbs: number | null;
-    protein: number | null;
-    fat: number | null;
-    created_at: string;
-    modified_at: string;
+  }
+  interface SecureSessionData {
+    idToken: string;
   }
 }
 export {};
