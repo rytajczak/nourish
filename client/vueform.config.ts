33713a2579c0e0ceb61d6de48b5c384b7aa9d73d
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-16 22:11:34 -0700

    update default headers

--- client/vueform.config.ts
+++ client/vueform.config.ts
@@ -1,8 +1,15 @@
 import en from "@vueform/vueform/locales/en";
 import vueform from "@vueform/vueform/dist/vueform";
 import { defineConfig } from "@vueform/vueform";
-
+import axios from "axios";
 import "@vueform/vueform/dist/vueform.css";
+
+axios.defaults.headers.put = {
+  "Content-Type": "application/json",
+};
+axios.defaults.headers.post = {
+  "Content-Type": "application/json",
+};
 
 export default defineConfig({
   theme: vueform,
