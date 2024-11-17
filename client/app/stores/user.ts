cb1d750b9f246758fc2f02df615ba54d63af4cc7
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-16 22:11:45 -0700

    update user store

--- client/app/stores/user.ts
+++ client/app/stores/user.ts
@@ -1,19 +1,18 @@
-import type { RecipePreview } from "~~/server/api/recipes/search.get";
-
 export const useUserStore = defineStore(
   "user",
   () => {
     const { user, clear } = useUserSession();
     const intolerances = ref<string[]>([]);
-    const savedRecipes = ref<RecipePreview[]>([]);
+    const savedRecipes = ref<any[]>([]);
     const dislikedIngredients = ref<string[]>([]);
 
     async function logOut() {
+      intolerances.value = [];
+      savedRecipes.value = [];
+      dislikedIngredients.value = [];
       await clear();
       navigateTo("/");
     }
-
-    async function fetchSavedRecipes() {}
 
     return { user, intolerances, dislikedIngredients, savedRecipes, logOut };
   },
