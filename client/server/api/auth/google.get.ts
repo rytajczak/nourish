cdd5c114e5be3f157bc8e0f5ca9648ad276142e8
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-17 15:04:27 -0700

    update signup

--- client/server/api/auth/google.get.ts
+++ client/server/api/auth/google.get.ts
@@ -3,36 +3,24 @@ import { User } from "#auth-utils";
 const config = useRuntimeConfig();
 
 /**
- * @description Handles the Google OAuth login and redirects to the dashboard page.
+ * @description Handles the Google OAuth login and redirects to the appropriate page.
  */
 export default defineOAuthGoogleEventHandler({
   async onSuccess(event, { user, tokens }) {
-    setCookie(event, "idToken", tokens.id_token, {
-      httpOnly: true,
-      path: "/",
+    await setUserSession(event, {
+      user: {
+        email: user.email,
+        username: user.name,
+        provider: "google",
+        picture: user.picture,
+      },
+      secure: { idToken: tokens.id_token },
     });
-    try {
-      await $fetch<User>(`${config.public.apiUrl}/users/me`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${tokens.id_token}`,
-        },
-      });
-      return sendRedirect(event, "/confirm");
-    } catch (error) {
-      await $fetch<User>(`${config.public.apiUrl}/users/`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${tokens.id_token}`,
-        },
-        body: {
-          email: user.email,
-          username: user.name,
-          picture: user.picture,
-          provider: "google",
-        },
-      });
-      return sendRedirect(event, "/onboarding");
-    }
+    return sendRedirect(event, "/confirm");
+  },
+
+  onError(event, error) {
+    console.error("Google OAuth error:", error);
+    return sendRedirect(event, "/");
   },
 });
