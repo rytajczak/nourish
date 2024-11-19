4a39d671907b91bb88156d6cf8e3b8cad1b23e9a
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-19 13:26:03 -0700

    update get so it stores creds

--- client/server/api/users/me/index.get.ts
+++ client/server/api/users/me/index.get.ts
@@ -1,9 +1,40 @@
 const apiUrl = useRuntimeConfig().public.apiUrl;
 
+interface GetMeResponse {
+  profile: {
+    diet: string;
+    calories: number;
+    protein: number;
+    carbs: number;
+    fat: number;
+  };
+  intolerances: string[];
+  dislikedIngredients: string[];
+  savedRecipes: any[];
+  spoonCredential: {
+    username: string;
+    hash: string;
+  };
+}
+
 export default defineEventHandler(async (event) => {
   const { secure } = await requireUserSession(event);
 
-  return await $fetch(`${apiUrl}/users/me`, {
+  const response = await $fetch<GetMeResponse>(`${apiUrl}/users/me`, {
     headers: { Authorization: `Bearer ${secure?.idToken}` },
   });
+
+  await setUserSession(event, {
+    secure: {
+      spoonName: response.spoonCredential.username,
+      spoonHash: response.spoonCredential.hash,
+    },
+  });
+
+  return {
+    profile: response.profile,
+    intolerances: response.intolerances,
+    dislikedIngredients: response.dislikedIngredients,
+    savedRecipes: response.savedRecipes,
+  };
 });
