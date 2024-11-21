cc32be799983fea6790c3b9a19a26e1e228a7f2f
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-20 23:28:35 -0700

    add types to utils

--- client/server/utils/spoonacular.ts
+++ client/server/utils/spoonacular.ts
@@ -7,3 +7,45 @@ export const spoonHeaders = {
   "x-rapidapi-key": key,
   "x-rapidapi-host": host,
 };
+
+export interface Nutrient {
+  name: string;
+  amount: number;
+  unit: string;
+  percentOfDailyNeeds: number;
+}
+
+export interface NutritionSummary {
+  nutrients: Nutrient[];
+}
+
+export interface RecipeValue {
+  readyInMinutes: number;
+  sourceUrl: string;
+  servings: number;
+  id: number;
+  title: string;
+  imageType: string;
+}
+
+export interface RecipeItem {
+  id: number;
+  slot: number;
+  position: number;
+  type: string;
+  value: RecipeValue;
+}
+
+export interface Day {
+  date: number;
+  day: string;
+  items: RecipeItem[];
+  nutritionSummary: NutritionSummary;
+  nutritionSummaryBreakfast: NutritionSummary;
+  nutritionSummaryLunch: NutritionSummary;
+  nutritionSummaryDinner: NutritionSummary;
+}
+
+export interface WeeklyPlan {
+  days: Day[];
+}
