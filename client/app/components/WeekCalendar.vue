167d62e625bc0f08080cf9b680bb1605789f4d88
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-20 21:54:06 -0700

    rename component

--- client/app/components/WeekCalendar.vue
+++ client/app/components/WeekCalendar.vue
@@ -4,19 +4,23 @@ const planner = usePlannerStore();
 const selectedDate = useState<Date>("selectedDate", () => new Date());
 const days = ref<Date[]>([]);
 
-onMounted(() => {
+onMounted(async () => {
   for (let i = 0; i < 7; i++) {
     const nextDate = new Date(selectedDate.value);
     nextDate.setDate(selectedDate.value.getDate() + i);
     days.value.push(nextDate);
   }
+  await planner.fetchWeek(new Date());
+});
+
+watch(selectedDate, async () => {
+  await planner.fetchWeek(new Date());
 });
 </script>
 
 <template>
-  <UCard :ui="{ body: 'p-0 sm:px-4 sm:py-0' }">
+  <UCard :ui="{ body: 'px-3 sm:px-3' }">
     <template #header>
-      {{ Math.floor(selectedDate.getTime() / 1000) }}
       <div class="flex items-center justify-center">
         <span class="text-xl font-semibold"
           >{{
@@ -38,25 +42,30 @@ onMounted(() => {
     <div class="flex items-center justify-center">
       <UCard
         v-for="day in days"
-        class="m-4 hidden flex-1 cursor-pointer transition-all duration-300 hover:scale-105 xl:block"
-        :ui="{ body: 'p-4 sm:p-4' }"
+        class="mx-3 hidden flex-1 cursor-pointer transition-all duration-150 hover:scale-105 lg:block"
         :class="{
-          'bg-[var(--ui-primary)] text-white':
+          'bg-foreground/90 text-background':
             selectedDate.getDate() == day.getDate(),
         }"
+        :ui="{ body: 'p-3 sm:p-3' }"
         @click="selectedDate = day"
       >
         <div class="flex flex-col items-center justify-center">
           <span class="text-xl font-semibold">{{
             day.toLocaleDateString("en-US", { day: "numeric" })
           }}</span>
-          {{ day.toLocaleDateString("en-US", { weekday: "short" }) }}
+          <span
+            :class="{
+              'text-neutral': selectedDate.getDate() == day.getDate(),
+            }"
+            >{{ day.toLocaleDateString("en-US", { weekday: "short" }) }}</span
+          >
         </div>
       </UCard>
-      <div class="flex items-center justify-center xl:hidden">
+      <div class="flex items-center justify-center lg:hidden">
         <UButton variant="ghost" icon="i-heroicons-chevron-left" />
         <span class="text-xl font-semibold">
-          {{ selectedDate.toLocaleDateString("en-US", { day: "numeric" }) }}
+          {{ selectedDate.toLocaleDateString("en-US", { weekday: "long" }) }}
         </span>
         <UButton variant="ghost" icon="i-heroicons-chevron-right" />
       </div>
