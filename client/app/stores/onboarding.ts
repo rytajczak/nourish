9f5cd9a2dd7ea8c7cd82b441eba2edc9bbf70ef3
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-22 11:59:15 -0700

    overhaul onboarding store

--- client/app/stores/onboarding.ts
+++ client/app/stores/onboarding.ts
@@ -1,32 +1,92 @@
+import { parse, stringify } from "zipson/lib";
+
+interface Data {
+  profile: {
+    diet: string;
+    calories: number;
+    protein: number;
+    carbs: number;
+    fat: number;
+  };
+  intolerances: string[];
+}
+
+interface Goals {
+  calories: number;
+  protein: number;
+  carbs: number;
+  fat: number;
+}
+
+// ma look! good practices!
+const TOTAL_STEPS = 3;
+
 export const useOnboardingStore = defineStore(
   "onboarding",
   () => {
-    const open = ref<boolean>(false);
-    const progress = ref<number>(0);
-    const data = ref({});
+    const open = ref(false);
+    const step = ref(0);
+    const progress = computed(() => step.value * (100 / TOTAL_STEPS));
+    const data = ref<Data>({
+      profile: {
+        diet: "",
+        calories: 0,
+        protein: 0,
+        carbs: 0,
+        fat: 0,
+      },
+      intolerances: [],
+    });
 
     function onboardUser() {
       open.value = true;
-      progress.value = 0;
-      data.value = {};
+      step.value = 0;
+      data.value.intolerances = [];
+    }
+
+    function setGoals(goals: Goals) {
+      data.value.profile.calories = goals.calories;
+      data.value.profile.protein = goals.protein;
+      data.value.profile.carbs = goals.carbs;
+      data.value.profile.fat = goals.fat;
+    }
+
+    function setDiet(diet: string) {
+      data.value.profile.diet = diet;
+    }
+
+    function setIntolerances(intolerances: string[]) {
+      data.value.intolerances = intolerances;
     }
 
     function nextStep() {
-      progress.value += 25;
+      step.value += 1;
     }
 
     function previousStep() {
-      progress.value -= 25;
+      step.value -= 1;
     }
 
     return {
       open,
+      step,
       progress,
       data,
       onboardUser,
+      setGoals,
+      setDiet,
+      setIntolerances,
       nextStep,
       previousStep,
     };
   },
-  { persist: true },
+  {
+    persist: {
+      storage: sessionStorage,
+      serializer: {
+        serialize: (value) => stringify(value),
+        deserialize: (value) => parse(value),
+      },
+    },
+  },
 );
