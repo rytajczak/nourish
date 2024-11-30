fca827ac3ab51b90969a716595bdfc266472f956
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-11-29 21:49:32 -0700

    update utils

--- client/app/utils/utils.ts
+++ client/app/utils/utils.ts
@@ -15,10 +15,10 @@ export function dateToString(date: Date): string {
  * @param date The date to format
  * @returns Date formatted as UNIX timestamp
  */
-export function dateToTimestamp(date: Date): string {
+export function dateToTimestamp(date: Date): number {
   const dayStart = new Date(date);
   dayStart.setHours(0, 0, 0, 0);
-  return String(dayStart.getTime() / 1000);
+  return dayStart.getTime() / 1000;
 }
 
 /**
